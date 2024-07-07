d92bc252655e0ff77d82f772b6a1c7774c9d3f07
Albertengo <145947798+Albertengo@users.noreply.github.com>
2024-07-07 16:35:42 -0300

    player mov (no funcional)

---
+++ Assets/Scripts/PlayerMov.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMov : MonoBehaviour
+{
+    private Rigidbody rb;
+    [SerializeField] private int speed;
+
+    public float jumpHeight = 1.0f;
+    public bool isGrounded;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Movement();   
+    }
+
+    private void Movement()
+    {
+        if (isGrounded == true) //mientras estes en el piso, se podra saltar, es para evitar volar
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+
+                rb.AddForce(Vector3.up * jumpHeight);
+                isGrounded = false;
+                Debug.Log("Estas saltando");
+            }
+            Debug.Log("Estas en el piso");
+        }
+
+        float horizontalInput = Input.GetAxis("Horizontal");
+        //float verticalInput = Input.GetAxis("Vertical");
+        Vector3 movement = new Vector3(horizontalInput, 0f, 0f); // verticalInput);
+
+        movement.Normalize();
+
+        transform.Translate(movement * Time.deltaTime * speed); //para moverse a los costados segun el player
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        }
+    }
+}
---
+++ Assets/Scripts/Sprites.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sprites : MonoBehaviour
+{
+    [SerializeField] private Camera _mainCamera;
+
+    void LateUpdate()
+    {
+        Vector3 cameraPosition = _mainCamera.transform.position;
+        cameraPosition.y = transform.position.y;
+        transform.LookAt(cameraPosition);
+        transform.Rotate(0f, 180f, 0f);
+    }
+}
