cfa31f8cef69cedada5516d67a98bdbcc2988f63
Albertengo <145947798+Albertengo@users.noreply.github.com>
2025-01-08 17:20:39 -0300

    cambiando colisiones
    para que el jugador no sea arrastrado por el enemigo

--- Assets/Scripts/Enemies/Enemies.cs
+++ Assets/Scripts/Enemies/Enemies.cs
@@ -8,9 +8,14 @@ public class Enemies : MonoBehaviour
 {
     //NOTA: hacer que la loot sea hijo del enemy para que siga moviendose despues de que se muera el enemigo y no se quede en el piso nomas
     // (para dar la ilusion de que el personaje se sigue moviendo y no está estático)
+    //NOTA2: usar el trigger dentro del prefab enemigo para que colisione con el player y sacarle vida
 
     [Header("Enemy properties")]
     [SerializeField] float speed;
+    [SerializeField] GameObject fx;
+
+    [SerializeField] MeshRenderer meshRenderer;
+
     [Header("Spawn properties")]
     public float Tvivo = 0f;
     public float TMAXvida = 20f;
@@ -50,7 +55,10 @@ public class Enemies : MonoBehaviour
         Life = Life - 1;//daño;
         if (Life <= 0)
         {
-            Destroy(gameObject);
+            //Destroy(gameObject);
+            fx.SetActive(false);
+            meshRenderer.enabled = false;
+            Debug.Log("enemigo desactivado");
             //MECANICA PARA CONTAR COMBO'???
             Loot();
         }
@@ -59,6 +67,7 @@ public class Enemies : MonoBehaviour
     {
         Vector2 position = transform.position; //chequea la posicion
         int dropsIndex = Random.Range(0, Drops.Length); //randomiza la loot
-        Instantiate(Drops[dropsIndex], position, Quaternion.identity); //instancia loot a recolectar
+        GameObject loot = Instantiate(Drops[dropsIndex], position, Quaternion.identity); //instancia loot a recolectar
+        this.transform.parent = loot.transform;
     }
 }
